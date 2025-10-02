861b4bc923bd08a0241acc2ff9f395568c88c2f9
Minato2404 <billykarma24@gmail.com>
2025-10-02 13:09:12 +0700

    Inisialisasi project devsecops-week2 dengan unit test dan workflow

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from src.app import add, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError, match="Tidak boleh bagi nol"):
+        divide(10, 0)
